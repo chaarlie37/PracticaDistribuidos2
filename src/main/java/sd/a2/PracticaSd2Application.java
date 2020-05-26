6b47cd7d475be5007afbf88d82c2568b0773dd89
chaarlie37 <carlos37sm@gmail.com>
2020-05-26 18:53:55 +0200

    Initial commit

---
+++ src/main/java/sd/a2/PracticaSd2Application.java
@@ -0,0 +1,13 @@
+package sd.a2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sd/a2/PracticaSd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
